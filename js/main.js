6e917f0cb691e38f41c8f48d6dee30007d49f36c
vikasdomakunti <vikasdomakunti@gmail.com>
2015-10-08 22:49:36 -0500

    Existing project commit.

---
+++ js/main.js
@@ -0,0 +1,79 @@
+// Hello.
+//
+// This is The Scripts used for ___________ Theme
+//
+//
+
+function main() {
+
+    (function () {
+        'use strict';
+
+        /* ==============================================
+         Testimonial Slider
+         =============================================== */
+        $('a.page-scroll').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top - 40
+                    }, 900);
+                    return false;
+                }
+            }
+        });
+
+        /*====================================
+         Show Menu on Book
+         ======================================*/
+        $(window).bind('scroll', function () {
+            var navHeight = $(window).height() - 91;
+            if ($(window).scrollTop() > navHeight) {
+                $('.navbar-default').addClass('on');
+            } else {
+                $('.navbar-default').removeClass('on');
+            }
+        });
+
+        $('body').scrollspy({
+            target: '.navbar-default',
+            offset: 80
+        });
+
+        $(document).ready(function () {
+        });
+
+        /*====================================
+         Portfolio Isotope Filter
+         ======================================*/
+        $(window).load(function() {
+            var $container = $('#timeline-pics');
+            $container.isotope({
+                filter: '*',
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false
+                }
+            });
+            $('.timeline-categories a').click(function() {
+                $('.timeline-categories .active').removeClass('active');
+                $(this).addClass('active');
+                var selector = $(this).attr('data-filter');
+                $container.isotope({
+                    filter: selector,
+                    animationOptions: {
+                        duration: 750,
+                        easing: 'linear',
+                        queue: false
+                    }
+                });
+                return false;
+            });
+
+        });
+    }());
+}
+main();
